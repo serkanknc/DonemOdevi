7e44be8b72c023d93de399a65185a67465f55407
serkanknc <serkankonukcu7@outlook.com>
2021-06-14 15:38:55 +0300

    son..

--- app/src/main/java/com/example/donemodevi/ui/AboutActivity.kt
+++ app/src/main/java/com/example/donemodevi/ui/AboutActivity.kt
@@ -10,6 +10,7 @@ class AboutActivity : AppCompatActivity() {
     lateinit var foto: ImageView
     lateinit var adsoyad: TextView
     lateinit var numara: TextView
+    lateinit var metin: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +21,12 @@ class AboutActivity : AppCompatActivity() {
         foto=findViewById(R.id.foto)
         adsoyad=findViewById(R.id.adsoyad)
         numara=findViewById(R.id.numara)
+        metin=findViewById(R.id.metin)
 
         foto.setBackgroundResource(R.mipmap.ppp)
         adsoyad.setText("AD SOYAD: SERKAN KONUKCU")
         numara.setText("ÖĞRENCİ NUMARASI: 191030025")
+        metin.setText("C# dili üzerinde çeşitli projeler yapmaktayım. PHP'de kendimi geliştiriyorum. PHP'ye yönelebilirim. Aslında şu dönem için meslek hayatımda ne yapmak istediğim konusu biraz karışık " +
+        "Eskiden sabit olan fikirlerimi biraz daha değiştirdim ve genişlettim, çünkü hayatta her şey olabiliyormuş.")
     }
 }
